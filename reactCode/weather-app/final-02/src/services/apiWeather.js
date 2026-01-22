4bcb12dc2105eb4bc5771b7a5ea83911908fb7a8
weiruiye <1207536154@qq.com>
2026-01-22 23:30:59 +0800

    Update apiWeather.js

--- reactCode/weather-app/final-02/src/services/apiWeather.js
+++ reactCode/weather-app/final-02/src/services/apiWeather.js
@@ -1,8 +1,7 @@
 const API_URL =
     'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
 
-// const API_KEY = 'YOUR_API_KEY';
-const API_KEY = '593945b73075d0e483765bf464247958';
+const API_KEY = 'YOUR_API_KEY';
 
 export async function getCurrentWeather(lat, lon) {
     const response = await fetch(
